86779ee2025d4bf3f47ff8885d6be19078d2c85a
Your Name <you@example.com>
2018-08-10 15:42:35 +0700

    remove some shit

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ if [ ! -f "$SERVER_NAME_FILE" ]; then
    echo "ServerName localhost" > $SERVER_NAME_FILE 
    a2enconf servername
 fi
+service apache2 start
+service apache2 reload
+service apache2 stop
 ###### Config mysql 
 ### create my.conf
 echo "# The MariaDB configuration file" >/etc/mysql/my.cnf
@@ -19,23 +22,27 @@ echo "port=3306" >> /etc/mysql/my.cnf
 echo "socket=/var/run/mysqld/mysqld.sock" >> /etc/mysql/my.cnf
 echo "[mysql]" >> /etc/mysql/my.cnf
 echo "no-auto-rehash" >> /etc/mysql/my.cnf
-echo "[myisamchk]" >> /etc/mysql/my.cnf
+# echo "[myisamchk]" >> /etc/mysql/my.cnf
 chmod 755 /var/run
 chmod 777 /var/lib/mysql
+mkdir /usr/local/bin/mysql
 chown -R mysql /var/lib/mysql
 chown -R mysql /var/run/mysqld
 chgrp -R mysql /var/lib/mysql
+chmod 777 /usr/local/bin/mysql
+chown -R mysql /usr/local/bin/mysql
 echo "########### Finish initial database ###################"
 service mysql start
 mysql -uroot -proot -e "CREATE USER 'root'@'%';"
+mysql -uroot -proot -e "CREATE USER 'root'@'::1';"
 mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES; "
 sleep 1
 service mysql stop
 ##init start file
 echo "#!/bin/bash" > /data.sh
-echo "service mysql start" >> data.sh
+echo "service mysql start" >> /data.sh
 echo "#!/bin/bash" > /ser.sh
-echo "apache2ctl -D FOREGROUND" >>ser.sh
+echo "apache2ctl -D FOREGROUND" >> /ser.sh
 sleep 1
 chmod 755 /data.sh
 chmod 755 /ser.sh
--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ __run() {
     /data.sh && /ser.sh
 }
 echo "############ init user database ########################"
-exec service mysql start
-exec mysql -uroot -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"
-exec mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWORD} ;"
-exec mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
+service mysql start
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;"
+echo mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+echo mysql -uroot -e "GRANT ALL ON PRIVILEGES ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
+mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
 sleep 1
-exec service mysql stop
+service mysql stop
 __run
 
 
