9510fa3306397d8efce219e2860806b4a25b1157
Your Name <you@example.com>
2018-08-10 10:25:23 +0700

    test

--- start.sh
+++ start.sh
@@ -9,12 +9,12 @@ chmod 755 /ser.sh
 chmod +x /data.sh
 chmod +x /ser.sh
 echo "############ init user database ########################"
-service mysql start
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"
-mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWORD} ;"
-mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
+exec service mysql start
+exec mysql -uroot -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"
+exec mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWORD} ;"
+exec mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
 sleep 1
-service mysql stop
+exec service mysql stop
 mysql_safe & apache2ctl -D FOREGROUND
 
 
