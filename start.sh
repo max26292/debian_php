42cb9feb798cba4c35dc3a3a6f64fe872037bc6d
Your Name <you@example.com>
2018-08-10 10:25:16 +0700

    test

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "#!/bin/bash" > /data.sh
-echo "service mysql start" >>data.sh
+exec echo "#!/bin/bash" > /data.sh
+exec echo "service mysql start" >> data.sh
 echo "#!/bin/bash" > /ser.sh
 echo "apache2ctl -D FOREGROUND" >>ser.sh
 sleep 1
@@ -9,14 +9,12 @@ chmod 755 /ser.sh
 chmod +x /data.sh
 chmod +x /ser.sh
 echo "############ init user database ########################"
-# service mysql start
-# mysql -uroot -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"
-# mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWORD} ;"
-# mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
-# sleep 1
-# service mysql stop
-# /data.sh & /ser.sh
-/ser.sh
-
+service mysql start
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWORD} ;"
+mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ; FLUSH PRIVILEGES;" 
+sleep 1
+service mysql stop
+mysql_safe & apache2ctl -D FOREGROUND
 
 
